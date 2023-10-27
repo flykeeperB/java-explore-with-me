8fa712715912ee463ee11adbe02f0b63abb61af8
Flykeeper <flykeeper@yandex.ru>
2023-10-27 12:01:22 +0500

    refactor: checkstyles #1

--- stats/stats-client/src/main/java/ru/practicum/statsClient/StatsClientImpl.java
+++ stats/stats-client/src/main/java/ru/practicum/statsClient/StatsClientImpl.java
@@ -1,7 +1,5 @@
 package ru.practicum.statsClient;
 
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
